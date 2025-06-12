f4a4c78f8ab14c4c9e97cfcdb101fc1a03414cf7
Jorim Soika <jorim.soika@gmx.de>
2025-06-12 15:18:37 +0200

    Feat: Added LiveLocation protobuffer structure

--- protobuf/messenger_pb2.pyi
+++ protobuf/messenger_pb2.pyi
@@ -1,3 +1,4 @@
+from google.protobuf import timestamp_pb2 as _timestamp_pb2
 from google.protobuf.internal import containers as _containers
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
@@ -20,38 +21,65 @@ class ServerAnnounce(_message.Message):
     feature: _containers.RepeatedCompositeFieldContainer[ServerAnnounce.Feature]
     def __init__(self, serverId: _Optional[str] = ..., feature: _Optional[_Iterable[_Union[ServerAnnounce.Feature, _Mapping]]] = ...) -> None: ...
 
+class User(_message.Message):
+    __slots__ = ("userId", "serverId")
+    USERID_FIELD_NUMBER: _ClassVar[int]
+    SERVERID_FIELD_NUMBER: _ClassVar[int]
+    userId: str
+    serverId: str
+    def __init__(self, userId: _Optional[str] = ..., serverId: _Optional[str] = ...) -> None: ...
+
 class ConnectClient(_message.Message):
     __slots__ = ("user", "portId")
-    class User(_message.Message):
-        __slots__ = ("userId", "serverId")
-        USERID_FIELD_NUMBER: _ClassVar[int]
-        SERVERID_FIELD_NUMBER: _ClassVar[int]
-        userId: str
-        serverId: str
-        def __init__(self, userId: _Optional[str] = ..., serverId: _Optional[str] = ...) -> None: ...
     USER_FIELD_NUMBER: _ClassVar[int]
     PORTID_FIELD_NUMBER: _ClassVar[int]
-    user: ConnectClient.User
+    user: User
     portId: int
-    def __init__(self, user: _Optional[_Union[ConnectClient.User, _Mapping]] = ..., portId: _Optional[int] = ...) -> None: ...
+    def __init__(self, user: _Optional[_Union[User, _Mapping]] = ..., portId: _Optional[int] = ...) -> None: ...
+
+class ChatMessage(_message.Message):
+    __slots__ = ("messageId", "timestamp", "author", "user", "text", "live_location")
+    MESSAGEID_FIELD_NUMBER: _ClassVar[int]
+    TIMESTAMP_FIELD_NUMBER: _ClassVar[int]
+    AUTHOR_FIELD_NUMBER: _ClassVar[int]
+    USER_FIELD_NUMBER: _ClassVar[int]
+    TEXT_FIELD_NUMBER: _ClassVar[int]
+    LIVE_LOCATION_FIELD_NUMBER: _ClassVar[int]
+    messageId: str
+    timestamp: _timestamp_pb2.Timestamp
+    author: User
+    user: User
+    text: str
+    live_location: LiveLocation
+    def __init__(self, messageId: _Optional[str] = ..., timestamp: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., author: _Optional[_Union[User, _Mapping]] = ..., user: _Optional[_Union[User, _Mapping]] = ..., text: _Optional[str] = ..., live_location: _Optional[_Union[LiveLocation, _Mapping]] = ...) -> None: ...
 
 class TypingEvent(_message.Message):
     __slots__ = ("user", "timestamp")
-    class User(_message.Message):
-        __slots__ = ("userId", "serverId")
-        USERID_FIELD_NUMBER: _ClassVar[int]
-        SERVERID_FIELD_NUMBER: _ClassVar[int]
-        userId: str
-        serverId: str
-        def __init__(self, userId: _Optional[str] = ..., serverId: _Optional[str] = ...) -> None: ...
     USER_FIELD_NUMBER: _ClassVar[int]
     TIMESTAMP_FIELD_NUMBER: _ClassVar[int]
-    user: TypingEvent.User
+    user: User
     timestamp: float
-    def __init__(self, user: _Optional[_Union[TypingEvent.User, _Mapping]] = ..., timestamp: _Optional[float] = ...) -> None: ...
+    def __init__(self, user: _Optional[_Union[User, _Mapping]] = ..., timestamp: _Optional[float] = ...) -> None: ...
 
 class TypingEvents(_message.Message):
     __slots__ = ("typing_events",)
     TYPING_EVENTS_FIELD_NUMBER: _ClassVar[int]
     typing_events: _containers.RepeatedCompositeFieldContainer[TypingEvent]
     def __init__(self, typing_events: _Optional[_Iterable[_Union[TypingEvent, _Mapping]]] = ...) -> None: ...
+
+class LiveLocation(_message.Message):
+    __slots__ = ("user", "timestamp", "expiry_at")
+    class Location(_message.Message):
+        __slots__ = ("latitude", "longitude")
+        LATITUDE_FIELD_NUMBER: _ClassVar[int]
+        LONGITUDE_FIELD_NUMBER: _ClassVar[int]
+        latitude: float
+        longitude: float
+        def __init__(self, latitude: _Optional[float] = ..., longitude: _Optional[float] = ...) -> None: ...
+    USER_FIELD_NUMBER: _ClassVar[int]
+    TIMESTAMP_FIELD_NUMBER: _ClassVar[int]
+    EXPIRY_AT_FIELD_NUMBER: _ClassVar[int]
+    user: User
+    timestamp: float
+    expiry_at: float
+    def __init__(self, user: _Optional[_Union[User, _Mapping]] = ..., timestamp: _Optional[float] = ..., expiry_at: _Optional[float] = ...) -> None: ...
