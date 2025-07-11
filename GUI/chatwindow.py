3cc3a2395e93e7bd424a5cb26be675bcc639a753
Jinx C <Jinxuanwolf@gmail.com>
2025-07-11 21:20:52 +0200

    documentdownload button recipient userid resipient serverid all infomation on textlabel show language selct button todo: api connection for documentdownload chatmessage language select

--- GUI/chatwindow.py
+++ GUI/chatwindow.py
@@ -9,7 +9,8 @@ import geocoder
 
 from client.typing_feature import TypingFeature
 from client.location_feature import LocationFeature
-from utils import parse_msg  # For parsing received messages
+# from client.chat_feature import ChatFeature
+from utils import parse_msg, colors  # For parsing received messages
 
 
 # Thread for listening to TypingEvent
@@ -35,6 +36,7 @@ class TypingListenerThread(QThread):
 
 class LocationListenerThread(QThread):
     location_received = pyqtSignal(float, float)  # lat, lon
+
 
     def __init__(self, locationFeature):
         super().__init__()
@@ -63,7 +65,9 @@ class LocationSharingThread(QThread):
 
 
 class ChatWindow(QMainWindow):
-    def __init__(self, typing_feature, location_feature):
+    log_signal = pyqtSignal(str)
+
+    def __init__(self, typing_feature, location_feature, chat_feature, translation, document):
         super().__init__()
         uic.loadUi(os.path.join(os.path.dirname(__file__), "chatwindow.ui"), self)
 
@@ -94,6 +98,12 @@ class ChatWindow(QMainWindow):
 
         self.typingLabel.clear()
 
+        self.log_signal.connect(self.status.append)
+        colors.gui_logger = self.thread_safe_log
+
+    def thread_safe_log(self, text):
+        self.log_signal.emit(text)
+
     def sendTypingEvent(self):
         self.typing_feature.send_typing_event()
         self.showTyping()
@@ -101,6 +111,7 @@ class ChatWindow(QMainWindow):
     def sendMessage(self):
         text = self.messageInput.text().strip()
         if text:
+            self.send_feature.send_message(recipientUserID(), recipientServerID(), text)
             self.chatDisplay.append("[You] " + text)
             self.messageInput.clear()
             self.chatDisplay.append("[Friend] reply test")
@@ -138,6 +149,23 @@ class ChatWindow(QMainWindow):
     def stopLocationSharing(self):
         self.locationFeature.stop_location_sharing()
         self.chatDisplay.append("[System] Location sharing stopped.")
+
+    def downloadDocument():
+        pass
+
+    def recipientUserID(self):
+        UserID = self.recipient_user_id.text().strip()
+        if UserID:
+            return UserID
+        else:
+            return "User123"
+        
+    def recipientServerID(self):
+        ServerID = self.recipient_server_id.text().strip()
+        if ServerID:
+            return ServerID
+        else:
+            return "server123"
 
     def closeEvent(self, event):
         # Gracefully stop threads when window is closed
--- utils/colors.py
+++ utils/colors.py
@@ -1,11 +1,17 @@
+gui_logger = None
+
 def red(text : str) -> None:
     print('\033[31m' + text + '\033[0m')
+    if gui_logger: gui_logger(text)
 
 def blue(text : str) -> None:
     print('\033[34m' + text + '\033[0m')
+    if gui_logger: gui_logger(text)
 
 def green(text : str) -> None:
     print('\033[32m' + text + '\033[0m')
+    if gui_logger: gui_logger(text)
 
 def yellow(text : str) -> None:
     print('\033[93m' + text + '\033[0m')
+    if gui_logger: gui_logger(text)
